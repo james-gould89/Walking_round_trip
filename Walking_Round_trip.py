a1013407fb723b4550913e8bc55a576f0244baf7
james-gould89 <79549361+james-gould89@users.noreply.github.com>
2024-01-28 20:02:28 +0000

    Update Walking_Round_trip.py
    updated search function to use vertex graph only.

--- Walking_Round_trip.py
+++ Walking_Round_trip.py
@@ -26,15 +26,15 @@ def build_heap_graph(graph):
   heap_graph = {}
   for location in graph.graph_dict:
     heap_graph[location]=[]
-    for edge in graph.get_vertex(location).get_edges():
-      heap_graph[location].append(tuple((edge, graph.get_vertex(location).get_edge_weight(edge))))
+    for edge in graph.get_edges(location):
+      heap_graph[location].append(tuple((edge, graph.get_edge_weight(location, edge))))
   return heap_graph
   ##print(heap_graph)
 
 ## return_time function will add the return times to all vertex set up inside the heap graph based on the minimum return time from that vertex back to the "office" vertex
 def return_time(graph, start):
   distances = {}
-  
+
   for vertex in graph:
     distances[vertex] = inf
     
@@ -83,38 +83,30 @@ def journey_search_bfs(graph, desired_time,  start_vertex = "office", target_val
   time_spent=0
   score=0
   vertex_path_and_time = [start_vertex, path,time_spent,score]
-  #print("vertex_path_and_time:\n",vertex_path_and_time)
   bfs_queue = [vertex_path_and_time]
-  #print("bfs_queue:\n",bfs_queue)
-  #visited = {k:0 for k in locations}
   count_proccessed = 0
   count_too_long = 0
   count_visited_twice = 0
   count_found = 0
   while bfs_queue:
     current_vertex, path, time_spent, score = bfs_queue.pop(0)
-    #print("path, bfs_queue:\n", path,".. ", bfs_queue)
-    #visited[current_vertex]=+1
-    #print("visited:\n",visited)
-    for neighbor, edge_weight in graph[current_vertex]:
+    for neighbor in graph.get_edges(current_vertex):
+      edge_weight = graph.get_edge_weight(current_vertex, neighbor)
       count_proccessed += 1
-      if path.count(neighbor) < 2: # and neighbor != target_value) or (path.count(neighbor) < 3 and neighbor == target_value):
-        if time_spent + edge_weight + vertex_graph.get_vertex(neighbor).get_return_time() < max_time:
+      if path.count(neighbor) < 2: 
+        if time_spent + edge_weight + graph.get_return_time(neighbor) < max_time:
           if neighbor is target_value and within_range(min_time, max_time, time_spent):
             result_journey = str(path + [neighbor])
             possible_journeys.append([result_journey , time_spent + edge_weight , score])
             count_found +=1
-            #print("Path found")
           else:
             if neighbor in path:
               bfs_queue.append([neighbor, path + [neighbor], time_spent + edge_weight, score])
             else:
               bfs_queue.append([neighbor, path + [neighbor], time_spent + edge_weight, score+values[neighbor]])
         else: 
-          #print("Spent too long {}".format(neighbor))
           count_too_long += 1
       else: 
-        #print("visited {} twice allready".format(neighbor))
         count_visited_twice += 1
   random.shuffle(possible_journeys)
   possible_journeys.sort(key=lambda x: x[2])
@@ -123,28 +115,9 @@ def journey_search_bfs(graph, desired_time,  start_vertex = "office", target_val
   print("proccessed {}/nSpent too long {}/n visited twice {}/n found {}".format(count_proccessed, count_too_long, count_visited_twice, count_found))
   print("Done")
 
-def journey_search(v_graph, min_time, max_time, vertex = "office", path = [], time_spent = 0, destination = "office"):
-  path.append(vertex)
-  print("Exploring {}".format(path)) ##check
-  print("time Spent {}".format(time_spent))
-  if vertex == destination and within_range(min_time, max_time, time_spent):
-    possible_journeys.appends([path, time_spent])
-    print("Path found")
-  else:
-    if path.count(vertex)>=3:
-      print("Visited {} 3 times".format(vertex))
-    else:
-      potentials = v_graph.get_vertex(vertex).get_edges()
-      for potential in potentials:
-        print("potential = {}".format(potential))
-        if time_spent + v_graph.get_vertex(vertex).get_edge_weight(potential) + v_graph.get_vertex(potential).get_return_time() > max_time:
-          print("Gone to far")
-        else:
-          journey_search(v_graph, min_time, max_time, potential, path, time_spent+v_graph.get_vertex(vertex).get_edge_weight(potential), destination)
-
 #find_journey(10)
 
-journey_search_bfs(heap_graph, 27)
+journey_search_bfs(vertex_graph, 27)
 #print(vertex_graph.get_vertex('Fairlands Valley park').get_return_time())
 
 
